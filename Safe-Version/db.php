45061cba5be09d9f63f8fab9d4b407c1b8a7be61
Sarah Aljuhani <444200927@student.ksu.edu.sa>
2026-04-27 22:05:17 +0300

    Update db.php

--- Safe-Version/db.php
+++ Safe-Version/db.php
@@ -1,7 +1,7 @@
 <?php
-//this file is used to connect our page to unsafe_db
+//this file is used to connect our page to safe_db
 
-$conn = mysqli_connect("localhost", "root", "", "unsafe_db");
+$conn = mysqli_connect("localhost", "root", "", "safe_db");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
