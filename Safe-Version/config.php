500ab654a4f578c945c7342c3b7381803c01ef1a
Sarah Aljuhani <444200927@student.ksu.edu.sa>
2026-05-04 18:23:48 +0300

    Create config.php

---
+++ Safe-Version/config.php
@@ -0,0 +1,8 @@
+<?php
+// Demo key for this project only.
+// In real web applications, the key should not be public.
+// It should be stored securely on the server.
+
+return [
+    "ENCRYPTION_KEY" => "12345678901234567890123456789012"
+];
