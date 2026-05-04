2474e1684560728c0c29dd9932e3a45559be2ecf
Sarah Aljuhani <444200927@student.ksu.edu.sa>
2026-05-04 18:37:13 +0300

    Update encryption key to a randomly generated value

--- Safe-Version/config.php
+++ Safe-Version/config.php
@@ -4,5 +4,5 @@
 // It should be stored securely on the server.
 
 return [
-    "ENCRYPTION_KEY" => "12345678901234567890123456789012"
+    "ENCRYPTION_KEY" => "baf2dedd369b489d5670b465c492bedfb34a0e3c6ecb3c71d7ae27f796515d17" //randomly generated key using: bin2hex(random_bytes(32));
 ];
